b8ab2f3eacb5d2ccd04f92282167c8e57989f32d
BAR-KEY <park96h@naver.com>
2022-01-05 18:13:53 +0900

    how to use ListView.builder & itemBuilder

--- lib/main.dart
+++ lib/main.dart
@@ -4,80 +4,57 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var a = 1;
+  var name = ['김영숙', '홍길동', '피자집', '박희문'];
+  var count = [0, 0, 0, 0];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-            appBar: AppBar(
-              title: Text('금호동3가 >'),
-              actions: [
-                IconButton(
-                  icon: Icon(Icons.search),
-                  onPressed: () => {},
-                  color: Colors.black,
-                ),
-                IconButton(
-                  icon: Icon(Icons.menu),
-                  onPressed: () => {},
-                  color: Colors.black,
-                ),
-                IconButton(
-                  icon: Icon(Icons.alarm),
-                  onPressed: () => {},
-                  color: Colors.black,
-                ),
-              ],
-              backgroundColor: Color.fromARGB(255, 255, 255, 255),
-              titleTextStyle: TextStyle(
-                color: Colors.black,
-                fontSize: 24,
-                fontWeight: FontWeight.w800,
-              ),
+      appBar: AppBar(title: Text('hi')),
+      body: ListView.builder(
+        itemCount: name.length,
+        itemBuilder: (c, i) {
+          return ListTile(
+            leading: Text(count[i].toString()),
+            title: Text(name[i]),
+            trailing: TextButton(
+              child: Text('좋아요'),
+              onPressed: () {
+                setState(() {
+                  count[i]++;
+                });
+              },
             ),
-            body: Container(
-              height: 150,
-              padding: EdgeInsets.all(20),
-              child: Row(
-                children: [
-                  Image.asset(
-                    'assets/doge.png',
-                    width: 150,
-                  ),
-                  Expanded(
-                    child: Container(
-                      padding: EdgeInsets.fromLTRB(30, 0, 10, 0),
-                      child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text('Doge 코인 팝니다.',
-                                style: TextStyle(
-                                    fontWeight: FontWeight.w700, fontSize: 20)),
-                            Text('달서구 도원동 . 끌올 10분 전',
-                                style: TextStyle(
-                                    fontWeight: FontWeight.w100,
-                                    fontSize: 14,
-                                    color: Color.fromARGB(255, 151, 151, 151))),
-                            Text('203원',
-                                style: TextStyle(
-                                    fontWeight: FontWeight.w500, fontSize: 18)),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [Icon(Icons.favorite), Text('4')],
-                            ),
-                          ]),
-                    ),
-                  ),
-                ],
-              ),
-              decoration: BoxDecoration(
-                border: Border.all(
-                  width: 1,
-                  color: Colors.orange,
-                ),
-              ),
-            )));
+          );
+        },
+      ),
+      bottomNavigationBar: BottomNavigator(),
+    ));
+  }
+}
+
+class BottomNavigator extends StatelessWidget {
+  const BottomNavigator({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 100,
+      child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+        Icon(Icons.phone, size: 30.0),
+        Icon(Icons.message, size: 30.0),
+        Icon(Icons.file_copy, size: 30.0),
+      ]),
+    );
   }
 }
