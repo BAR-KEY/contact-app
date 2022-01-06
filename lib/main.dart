5532ea3443bf3f4419cef02db61a52f194a323be
BAR-KEY <park96h@naver.com>
2022-01-06 17:46:05 +0900

    dialogUI 완료누르면 닫히게, dialog의 input에 빈칸이면 추가안되게 구현완료

--- lib/main.dart
+++ lib/main.dart
@@ -15,12 +15,31 @@ class _MyAppState extends State<MyApp> {
   var a = 1;
   var name = ['김영숙', '홍길동', '피자집', '박희문'];
   var count = [0, 0, 0, 0];
+  var inputData = '';
+  addData(enterName) {
+    if (enterName.length > 0) {
+      setState(() {
+        name.add(enterName);
+        count.add(0);
+      });
+    }
+  }
+
+  add() {
+    setState(() {
+      a++;
+    });
+    return a;
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      appBar: AppBar(title: Text('hi')),
+      appBar: AppBar(
+          title: Text(
+        a.toString(),
+      )),
       body: ListView.builder(
         itemCount: name.length,
         itemBuilder: (c, i) {
@@ -39,7 +58,83 @@ class _MyAppState extends State<MyApp> {
         },
       ),
       bottomNavigationBar: BottomNavigator(),
+      floatingActionButton: Builder(builder: (context) {
+        return DialogUI(
+            a: a,
+            name: name,
+            add: add,
+            count: count,
+            inputData: inputData,
+            addData: addData);
+      }),
     ));
+  }
+}
+
+class DialogUI extends StatelessWidget {
+  DialogUI(
+      {Key? key,
+      this.a,
+      this.name,
+      this.add,
+      this.count,
+      this.inputData,
+      this.addData})
+      : super(key: key);
+  var a, name, add, count, inputData, addData;
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+        child: Text('hi'),
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (context) {
+                return Dialog(
+                    child: SizedBox(
+                  width: 400,
+                  height: 300,
+                  child: Padding(
+                    padding: const EdgeInsets.all(40.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Contact',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.w600),
+                        ),
+                        TextField(
+                          onChanged: (text) {
+                            inputData = text;
+                          },
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            TextButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: Text('Cancel')),
+                            TextButton(
+                                onPressed: () {
+                                  add();
+                                  addData(inputData);
+                                  Navigator.pop(context);
+                                },
+                                child: Text('OK')),
+                          ],
+                        ),
+                        Text(a.toString())
+                      ],
+                    ),
+                  ),
+                ));
+              });
+        });
   }
 }
 
